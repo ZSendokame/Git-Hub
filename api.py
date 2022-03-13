1787ae4b132263816211d000b0d3d24242eeb260
ZSendokame <sendokame@gmail.com>
2022-03-13 15:42:53 -0300

    Update api.py

--- api.py
+++ api.py
@@ -6,10 +6,13 @@ app = Flask(__name__, template_folder='./source', static_folder='./source/static
 @app.get('/')
 def root():
     get = requests.get('https://api.github.com/users/' + request.args.get('username'))
-    username = get.json()['name']
-    biography = get.json()['bio']
-    followerCount = get.json()['followers']
 
-    return render_template('index.html', username=username, biography=biography, followerCount=followerCount)
+    return render_template(
+        'index.html',
+        username=get.json()['name'],
+        biography=get.json()['bio'],
+        followerCount=get.json()['followers'],
+        repositoryCount=get.json()['public_repos']
+    )
 
 app.run()
